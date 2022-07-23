a0ed444a4274458d3b137f4b383d805ff9ee4cce
Sorawit Trutsat <sorawit.tr@gmail.com>
2022-07-23 20:07:11 +0700

    first commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Sorawit Trutsat on 23/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
